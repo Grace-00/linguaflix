1898d843777bedafe259e7667fad4e52d4469ad0
Grazia Palombella <grazia.palombella@gmail.com>
2024-03-07 15:31:34 +0100

    feat: moved types for better readability

---
+++ types.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cats = void 0;
+//mock data for now
+exports.cats = [
+    {
+        name: "fru",
+        age: 3,
+    },
+    {
+        name: "simba",
+        age: 1,
+    },
+];
---
+++ types.ts
@@ -0,0 +1,16 @@
+export type Cat = {
+  name: string;
+  age: number;
+};
+
+//mock data for now
+export const cats: Cat[] = [
+  {
+    name: "fru",
+    age: 3,
+  },
+  {
+    name: "simba",
+    age: 1,
+  },
+];
