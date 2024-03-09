f793cc148fc0176afdd1bbb977c1e0fab647c509
Grazia Palombella <grazia.palombella@gmail.com>
2024-03-09 14:51:21 +0100

    feat: update JS

--- router.js
+++ router.js
@@ -4,6 +4,6 @@ const express_1 = require("express");
 const types_1 = require("./types");
 const router = (0, express_1.Router)();
 router.get("/", (req, res) => {
-    res.send(types_1.cats);
+    res.send(types_1.comments);
 });
 exports.default = router;
