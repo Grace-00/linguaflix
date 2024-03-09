5432f160756e53a5770121e8c40052a327f0f42f
Grazia Palombella <grazia.palombella@gmail.com>
2024-03-09 15:33:12 +0100

    feat: WIP POST

--- router.ts
+++ router.ts
@@ -1,10 +1,24 @@
 import { Router, Request, Response } from "express";
 import { comments } from "./types";
+import { uuid } from "uuidv4";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send(comments);
+});
+
+router.post("/comments", (req: Request, res: Response) => {
+  //this is so so sooo cool. Next step: fix types and make image keys not required
+  comments.push({
+    content: "new content",
+    createdAt: String(Date.now()),
+    replies: [],
+    id: Number(uuid()),
+    score: Number(uuid()),
+    user: { image: { png: "hello", webp: "webp" }, username: "pippo" },
+  });
+  res.send({ comments });
 });
 
 export default router;
