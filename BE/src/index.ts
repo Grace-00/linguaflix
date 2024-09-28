8791ea8d01e7f63d565b03f88f5ecb313502c083
Grazia Palombella <grazia.palombella@gmail.com>
2024-09-28 19:30:04 +0200

    feat: render comments on FE with Next

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,9 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import morgan from "morgan";
+import cors from "cors";
 import router from "./router";
 const app: Express = express();
-const port = 3000;
+const port = 5001;
 
+app.use(cors()); //middleware to make sure that different ports can talk to each other
 app.use(morgan("dev")); //middleware for logging on dev (ex. GET)
 app.use(express.json()); //middleware that allows a client to send us json
 app.use(express.urlencoded({ extended: true })); //middleware that allows query string to be encoded and decoded, to be in an object rather than strings
