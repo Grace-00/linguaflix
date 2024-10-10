1dcb9c7a3e541bfd8995c3ba4956eb292c5c1377
Grazia Palombella <grazia.palombella@gmail.com>
2024-10-10 22:22:34 +0200

    chore: config for BE deployment

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -2,9 +2,10 @@ import express, { Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import router from "./router.js";
-
+import * as dotenv from "dotenv";
+import config from "./config/index.js";
+dotenv.config();
 const app: Express = express();
-const port = 5001;
 
 app.use(cors()); //middleware to make sure that different ports can talk to each other
 app.use(morgan("dev")); //middleware for logging on dev (ex. GET)
@@ -12,6 +13,6 @@ app.use(express.json()); //middleware that allows a client to send us json
 app.use(express.urlencoded({ extended: true })); //middleware that allows query string to be encoded and decoded, to be in an object rather than strings
 app.use("/api", router); //this is used to create modular routes
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(config.port, () => {
+  console.log(`[server]: Server is running at http://localhost:${config.port}`);
 });
